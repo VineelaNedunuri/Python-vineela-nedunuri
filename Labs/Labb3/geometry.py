e1214c60f7cabca32da07af1b4c226a30f8ea03f
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2022-10-09 20:29:52 +0200

    lab3

--- Labs/Labb3/geometry.py
+++ Labs/Labb3/geometry.py
@@ -18,9 +18,6 @@ class Geometry(): # create main class as Geometry
         if not isinstance(value, (float,int)):
             raise TypeError(f"value must be an int or float, not {type(value).__name__}")
         
-        if value == "str":
-            raise ValueError(f" x must be numbers not letter")
-        
         self._x = value
 
     # getter     
@@ -36,35 +33,37 @@ class Geometry(): # create main class as Geometry
         if not isinstance(value, (float,int)):
             raise TypeError(f"value must be an int or float, not {type(value).__name__}")
             
-        if value == "str":
-            raise ValueError(f" x must be numbers not letter")
-        
         self._y = value
-    
-    def __repr__(self):
-        return f"Geometry(x = {self.x}, y = {self.y})"
-    
+
+    # overrided dunder string method
     def __str__(self):
         return f"Geomerty with co-ordinates  x and y are {self.x}, {self.y}"
 
+    # overrided dunder reper method    
+    def __repr__(self):
+        return f"Geometry(x = {self.x}, y = {self.y})"
     
-    def translate(self, new_x, new_y): 
-        try:
+   
+    # translate method that makes it possible to move x and y 
+    def translate(self, new_x, new_y):
+            self._x +=  new_x
+            self._y +=  new_y
             
-            if new_x or new_y == str:
-                print(" x  and y must be numbers not letter")
-        
-        except ValueError  as err:
-            print(err)
-        
-        self._x = self._x + new_x
-        self._y = self._y + new_y
+            if not isinstance(self._x, (float,int)):
+                raise TypeError(f"value must be an int or float, not {type(self._x).__name__}")
 
+            
+            if type(new_x) or type(new_y) == str:
+                raise ValueError(f" x  and y must be numbers not letter")
 
+             
+            return self._x, self._y          
         
+
+       
 
 g1 = Geometry(1,2)
 print(g1)
 
-g1.translate("tre",2)
+g1.translate(7,2)
 print(g1)
